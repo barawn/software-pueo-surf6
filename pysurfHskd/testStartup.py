1ac058a24c7ea35c9855d444aab3b02e2a2db78e
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-05-08 15:12:22 -0400

    ok just stop in WAIT_CIN_ACTIVE for now

--- pysurfHskd/testStartup.py
+++ pysurfHskd/testStartup.py
@@ -122,7 +122,7 @@ startup = StartupHandler(LOG_NAME,
                          surf,
                          clk,
                          clkrst,
-                         StartupHandler.StartupState.STARTUP_FINISH,
+                         StartupHandler.StartupState.WAIT_CIN_ACTIVE,
                          tickFifo)
 # sigh stupidity
 def runHandler(fd, mask):
